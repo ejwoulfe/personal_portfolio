f006edb98d55388c1b297add881d954df78c9cee
Ed <edwoulfe@gmail.com>
2019-08-28 20:53:22 -0500

    Dark and Light Theme button working. changing color scheme and background images in the jumbotron.

--- src/Components/Jumbotron/change-color-button.jsx
+++ src/Components/Jumbotron/change-color-button.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import Button from "react-bootstrap/Button";
+import darkBackgroundImage from "../../Assets/mountain.jpg";
+import lightBackgroundImage from "../../Assets/winter.jpg";
 
 class ChangeColorTheme extends Component {
   constructor(props) {
@@ -15,19 +17,38 @@ class ChangeColorTheme extends Component {
   }
   componentDidUpdate() {
     if (this.state.lightTheme) {
-      document.getElementsByClassName("App").style.backgroundColor = "#FFFFFF";
-      document.getElementsByClassName("App").style.color = "#19171C";
+      document.getElementsByClassName("App")[0].style.backgroundColor =
+        "#FFFFFF";
+      document.getElementsByClassName("App")[0].style.color = "#19171C";
+      document.getElementById(
+        "jumbotron_container"
+      ).style.backgroundImage = `url(${lightBackgroundImage})`;
     } else {
-      document.getElementsByClassName("App").style.backgroundColor = "#111820";
-      document.getElementsByClassName("App").style.color = "#b2123b";
+      document.getElementsByClassName("App")[0].style.backgroundColor =
+        "#111820";
+      document.getElementsByClassName("App")[0].style.color = "#b2123b";
+      document.getElementById(
+        "jumbotron_container"
+      ).style.backgroundImage = `url(${darkBackgroundImage})`;
     }
+  }
+  componentDidMount() {
+    document.getElementById(
+      "jumbotron_container"
+    ).style.backgroundImage = `url(${lightBackgroundImage})`;
+    document.getElementsByClassName("App")[0].style.backgroundColor = "#FFFFFF";
+    document.getElementsByClassName("App")[0].style.color = "#19171C";
   }
 
   render() {
     return (
       <div id="color_theme_container">
+        <p>
+          Want a {this.state.lightTheme ? "dark" : "light"} theme? Click button
+          to change it.{" "}
+        </p>
         <Button onClick={this.handleChangeTheme}>
-          {this.state.lightTheme ? "Light Theme" : "Dark Theme"}
+          {this.state.lightTheme ? "Dark Theme" : "Light Theme"}
         </Button>
       </div>
     );
