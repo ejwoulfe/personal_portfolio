ecc536c4c5acf64856226e1efc71f52fa90f5fd3
Ed <edwoulfe@gmail.com>
2019-10-03 18:23:14 -0500

    Fixed about me media queries and containers so the description and the photo are next to each other and are flexible.

--- src/Components/About/about.jsx
+++ src/Components/About/about.jsx
@@ -7,27 +7,27 @@ class About extends Component {
   render() {
     return (
       <React.Fragment>
-        <div id="picture" className="col-sm-5">
-          <img src={selfie} alt="Me"></img>
-        </div>
-        <div id="about_description" className="col-sm-4">
-          <p>
-            My name is Ed Woulfe. I am a recent college graduation with a degree
-            in Computer Science.
-          </p>
-          <p>
-            During my studies I discovered my passion for web development. Ever
-            since, I have been studying and practicing in order to expand my web
-            dev skills and knowledge.
-          </p>
-          <p>
-            I am excited to keep learning more, improving my skills and working
-            with others to make respectable and beautiful websites.
-          </p>
-          <p>
-            All contact information can be found on my resume. A link is
-            provided at the top of the page. Thank you!
-          </p>
+        <div id="about_div" className="col-sm-9">
+          <img id="picture" src={selfie} alt="Me"></img>
+          <div id="about_description">
+            <p>
+              My name is Ed Woulfe. I am a recent college graduation with a
+              degree in Computer Science.
+            </p>
+            <p>
+              During my studies I discovered my passion for web development.
+              Ever since, I have been studying and practicing in order to expand
+              my web dev skills and knowledge.
+            </p>
+            <p>
+              I am excited to keep learning more, improving my skills and
+              working with others to make respectable and beautiful websites.
+            </p>
+            <p>
+              All contact information can be found on my resume. A link is
+              provided at the top of the page. Thank you!
+            </p>
+          </div>
         </div>
       </React.Fragment>
     );
