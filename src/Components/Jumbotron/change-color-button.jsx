77d48d89ed155d61fa886203ae47478fcc7f0b6c
Ed <edwoulfe@gmail.com>
2019-11-18 19:29:20 -0600

    Cleaned up code and made a new svg with dark themes colors.
    Put the two different themes css changes into functions to reduce clutter and better maintence. All changes can be made inside the functions now instead of several different areas.

--- src/Components/Jumbotron/change-color-button.jsx
+++ src/Components/Jumbotron/change-color-button.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import Button from "react-bootstrap/Button";
+import skillsIconLight from "../../Assets/light-cogs-solid.svg";
+import skillsIconDark from "../../Assets/dark-cogs-solid.svg";
 
 class ChangeColorTheme extends Component {
   constructor(props) {
@@ -9,54 +11,68 @@ class ChangeColorTheme extends Component {
     };
     this.handleChangeTheme = this.handleChangeTheme.bind(this);
   }
+  changeToLightTheme() {
+    // Changes to all letters within the Application
+    document.getElementsByClassName("App")[0].style.color = "#19171C";
+    document.getElementsByClassName("App")[0].style.backgroundColor = "white";
+    document.getElementById("jumbotron_container").style.color = "white";
+
+    // Changes background colors of some containers.
+    document.getElementById("skills_container").style.backgroundColor =
+      "#e6e6e6";
+    document.getElementById("about_container").style.backgroundColor =
+      "#e6e6e6";
+
+    // Changes the jumbotron button
+    document.getElementsByClassName("btn")[0].style.color = "white";
+    document.getElementsByClassName("btn")[0].style.borderColor = "white";
+
+    // Changes the svg icons to the light cogwheel
+    document.getElementsByClassName("svg")[0].src = skillsIconLight;
+    document.getElementsByClassName("svg")[1].src = skillsIconLight;
+
+    // Changes all other buttons in the application.
+    for (var i = 1; i < document.getElementsByClassName("btn").length; i++) {
+      document.getElementsByClassName("btn")[i].style.color = "white";
+      document.getElementsByClassName("btn")[i].style.borderColor = "black";
+    }
+  }
+  changeToDarkTheme() {
+    // Changes to all letters within the Application
+    document.getElementsByClassName("App")[0].style.backgroundColor = "#111820";
+    document.getElementsByClassName("App")[0].style.color = "#b2123b";
+    document.getElementById("jumbotron_container").style.color = "#b2123b";
+
+    // Changes background colors of some containers.
+    document.getElementById("skills_container").style.backgroundColor =
+      "#1b1e24";
+    document.getElementById("about_container").style.backgroundColor =
+      "#1b1e24";
+
+    // Changes the svg icons to the dark cogwheel
+    document.getElementsByClassName("svg")[0].src = skillsIconDark;
+    document.getElementsByClassName("svg")[1].src = skillsIconDark;
+
+    // Changes all other buttons in the application.
+    for (var j = 0; j < document.getElementsByClassName("btn").length; j++) {
+      document.getElementsByClassName("btn")[j].style.borderColor = "#b2123b";
+      document.getElementsByClassName("btn")[j].style.color = "#b2123b";
+    }
+  }
 
   handleChangeTheme() {
     this.setState({ lightTheme: !this.state.lightTheme });
   }
   componentDidUpdate() {
     if (this.state.lightTheme) {
-      document.getElementsByClassName("App")[0].style.color = "#19171C";
-      document.getElementsByClassName("App")[0].style.backgroundColor = "white";
-      document.getElementById("jumbotron_container").style.color = "white";
-      document.getElementById("skills_container").style.backgroundColor =
-        "#e6e6e6";
-      document.getElementById("about_container").style.backgroundColor =
-        "#e6e6e6";
-      document.getElementsByClassName("btn")[0].style.color = "white";
-      document.getElementsByClassName("btn")[0].style.borderColor = "white";
-      for (var i = 1; i < document.getElementsByClassName("btn").length; i++) {
-        document.getElementsByClassName("btn")[i].style.color = "white";
-        document.getElementsByClassName("btn")[i].style.borderColor = "black";
-      }
+      this.changeToLightTheme();
     } else {
-      document.getElementById("about_container").style.backgroundColor =
-        "#1b1e24";
-      document.getElementsByClassName("App")[0].style.backgroundColor =
-        "#111820";
-      document.getElementById("skills_container").style.backgroundColor =
-        "#1b1e24";
-      document.getElementsByClassName("App")[0].style.color = "#b2123b";
-
-      document.getElementById("jumbotron_container").style.color = "#b2123b";
-
-      for (var j = 0; j < document.getElementsByClassName("btn").length; j++) {
-        document.getElementsByClassName("btn")[j].style.borderColor = "#b2123b";
-        document.getElementsByClassName("btn")[j].style.color = "#b2123b";
-      }
+      this.changeToDarkTheme();
     }
   }
   componentDidMount() {
-    document.getElementsByClassName("App")[0].style.color = "#19171C";
-    document.getElementById("skills_container").style.backgroundColor =
-      "#e6e6e6";
-    document.getElementsByClassName("btn")[0].style.color = "white";
-    document.getElementsByClassName("btn")[0].style.borderColor = "white";
-    for (var k = 1; k < document.getElementsByClassName("btn").length; k++) {
-      document.getElementsByClassName("btn")[k].style.color = "white";
-      document.getElementsByClassName("btn")[k].style.borderColor = "black";
-    }
-
-    document.getElementById("jumbotron_container").style.color = "white";
+    // Default will be light theme
+    this.changeToLightTheme();
   }
 
   render() {
