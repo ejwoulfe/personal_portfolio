a7d9b1b8e77e369b88819e9bce905aa68bf20676
Ed <edwoulfe@gmail.com>
2019-12-15 18:01:46 -0600

    Removed linkedIn until future date.

--- src/Components/Jumbotron/title-and-links.jsx
+++ src/Components/Jumbotron/title-and-links.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faFile } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import ChangeColorTheme from "./change-color-button";
@@ -19,16 +19,6 @@ function TitleAndLinks() {
             <FontAwesomeIcon icon={faGithub} size="2x" />
           </a>
           <h5>Github</h5>
-        </span>
-        <span>
-          <a
-            href="https://www.linkedin.com/in/edwoulfe/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FontAwesomeIcon icon={faLinkedin} size="2x" />
-          </a>
-          <h5>Linkedin</h5>
         </span>
         <span>
           <a href={pdf} target="_blank" rel="noopener noreferrer" download>
